90695e90e8ce5d1c7b5c795711228f0cb8f55d74
PNT-01 <sjh98127@gmail.com>
2024-10-28 14:24:17 +0900

    no message

--- src/components/SpecialAuction/SAlist.js
+++ src/components/SpecialAuction/SAlist.js
@@ -67,10 +67,12 @@
               <SAitem
                 key={index}
                 imageSrc={imageSrc}
+                price={auction.startingPrice}
                 title={auction.productName}
-                linkText={auction.auctionStatus}
                 auctionDate={formatDateTime(auction.startingLocalDateTime)}
                 auctionTime={formatAuctionTimeRange(auction.startingLocalDateTime, auction.endingLocalDateTime)}
+                linkText="바로가기"
+                alertText="* 알림은 경매 시작 30분 전에 발송됩니다."
                 handleGoButtonClick={() => handleGoButtonClick(auction)}
                 handleAlertButtonClick={() => { togglePopup('showAlertPopup', true); handleAlertButtonClick(auction); }}
               />
@@ -143,37 +145,20 @@
       if (selectedAuction) {
         const interval = setInterval(() => {
           const now = new Date();
-          const auctionStartTime = new Date(selectedAuction.startingLocalDateTime);
           const auctionEndTime = new Date(selectedAuction.endingLocalDateTime);
+          const timeDifference = auctionEndTime - now;
     
-          let timeDifference;
-
-          if (now < auctionStartTime) {
-            // 경매 시작 전
-            timeDifference = auctionStartTime - now;
+          if (timeDifference > 0) {
             setRemainingTime(timeDifference);
-          } else if (now >= auctionStartTime && now < auctionEndTime) {
-            // 경매 진행 중
-            timeDifference = auctionEndTime - now;
-            setRemainingTime(timeDifference);
-
-             // 경매가 시작되면 BuyerWaitPopup을 닫고 BuyerAuctionScreen으로 전환
-              if (!popupState.showBuyerAuctionScreen && popupState.showBuyerPopup) {
-                togglePopup('showBuyerPopup', false); // BuyerWaitPopup 닫기
-                togglePopup('showBuyerAuctionScreen', true); // BuyerAuctionScreen 열기
-              }
           } else {
-            // 경매 종료 후
-            timeDifference = -1;
-            setRemainingTime(timeDifference);
             setHasAuctionEnded(true);
-            clearInterval(interval); // 경매 종료 후 타이머 정리
+            clearInterval(interval);
           }
         }, 1000);
     
         return () => clearInterval(interval); // 컴포넌트 언마운트 시 타이머 정리
       }
-    }, [selectedAuction]);
+    }, [selectedAuction, hasAuctionEnded]);
   
 
     // 구매자 팝업 닫기 + 웹 소켓 연결 해제
