75ea64ff76af6d9d03faacde6552fad3edce9193
PNT-01 <sjh98127@gmail.com>
2024-10-28 13:06:29 +0900

    24.10.28 실시간 경매 컴포넌트 스타일 수정

--- src/components/Mypage/MypageWalletRecord.js
+++ src/components/Mypage/MypageWalletRecord.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React from 'react'
 import '../../css/Mypage/Mypage.css';
 
 export const MypageWalletRecord = () => {
   return (
     <>
-        <div className='test1'>MypageWalletRecord</div>
-        <div className='test1'>MypageWalletRecord</div>
-        <div className='test1'>MypageWalletRecord</div>
-        <div className='test1'>MypageWalletRecord</div>
-        <div className='test1'>MypageWalletRecord</div>
-        <div className='test1'>MypageWalletRecord</div>
-        <div className='test1'>MypageWalletRecord</div>
-        <div className='test1'>MypageWalletRecord</div>
-        <div className='test1'>MypageWalletRecord</div>
-        <div className='test1'>MypageWalletRecord</div>
+        <div className='Mypage_WalletCard'>
+            <div className='Mypage_WalletCardLeft'>아이콘 자리</div>
+            <div className='Mypage_WalletCardRight'>
+                <div className='Mypage_WalletLine'>
+                    <div className='Mypage_WalletCardLine1'>
+                        <div>[기록 타입]</div>
+                        <div>[기록 시간]</div>
+                    </div>
+                    <div className='Mypage_WalletCardLine2'>
+                        <div>[기록 메시지]</div>
+                    </div>
+                    <div className='Mypage_WalletCardLine3'>
+                        <div>[크레딧 변동 기록]</div>
+                        <div>[완료 후 크레딧]</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </>
   )
 }
--- src/components/SpecialAuction/SAitem.js
+++ src/components/SpecialAuction/SAitem.js
@@ -5,16 +5,23 @@ function SAitem({ imageSrc, title, auctionDate, auctionTime, linkText, alertText
 
   return (
     <div className="SAauctionItem">
-      <img src={imageSrc} alt={title} className="SAauctionImage" />
-      <div className="SAauctionDetails">
-        <h3>{title}</h3>
-        <p>경매 날짜: {auctionDate}</p>
-        <p>경매 시간: {auctionTime}</p>
-        <p className="SAalertText">* 알림은 경매 시작 30분 전에 발송됩니다.<br/>* 실시간 경매는 참여 인원수가 5000명으로 제한됩니다.</p>
+      <div className='SAauctionCard'>
+        <img src={imageSrc} alt={title} className="SAauctionImage" />
+        <div className="SAauctionDetails">
+          <div className='SAauctionDetailsTitle'>
+            <h3>{title}</h3>
+          </div>
+          <div className='SAauctionDetailsDate'>
+            <p>경매 날짜: {auctionDate}</p>
+            <p>경매 시간: {auctionTime}</p>
+          </div>
+          {/* <p className="SAalertText">* 알림은 경매 시작 30분 전에 발송됩니다.<br/>* 실시간 경매는 참여 인원수가 5000명으로 제한됩니다.</p> */}
+        
+        <div className="SAauctionButtons">
+          <button className="SAgoButton" onClick={handleGoButtonClick}>바로가기</button>
+          <button className="SAalertButton" onClick={handleAlertButtonClick}>알림신청</button>
+        </div>
       </div>
-      <div className="SAauctionButtons">
-        <button className="SAgoButton" onClick={handleGoButtonClick}>바로가기</button>
-        <button className="SAalertButton" onClick={handleAlertButtonClick}>알림신청</button>
       </div>
     </div>
   );
