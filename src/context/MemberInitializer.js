0dc3d2e3f837a4f49c7a4bf05adabe1e326de5a7
BITCAMP\BIT <arisu0304@naver.com>
2024-10-30 11:49:15 +0900

    241030 로그인 문제 수정

--- src/context/MemberInitializer.js
+++ src/context/MemberInitializer.js
@@ -1,13 +1,24 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { fetchMember } from "../apis/etc2_memberapis/memberApis"; // fetchMember 액션 임포트
+import { useDispatch, useSelector } from "react-redux";
+import { fetchMember, checkLogin } from "../apis/etc2_memberapis/memberApis"; // fetchMember 액션 임포트
 
 function MemberInitializer() {
+
     const dispatch = useDispatch();
 
     useEffect(() => {
-        // 멤버 정보를 초기화하는 액션을 디스패치
-        dispatch(fetchMember());
+        const initializeMember = async () => {
+            try {
+                const result = await dispatch(checkLogin()).unwrap(); // checkLogin이 성공적으로 완료되었을 때만 다음 코드 실행
+                if (result.item === "ROLE_USER") { // 로그인 상태 확인
+                    dispatch(fetchMember());
+                }
+            } catch (error) {
+                console.warn("로그인되지 않은 상태입니다.", error);
+            }
+        };
+
+        initializeMember();
     }, [dispatch]);
 
     return null; // UI가 필요 없으므로 null 반환
