7c4fed3e9d5406359c6b061d26aa82c395cc5619
LAPTOP-JEE40NVN\arisu <arisuww0424@gmail.com>
2024-11-03 19:48:30 +0900

    241103

--- src/components/SpecialAuction/AuctionEndPopup.js
+++ src/components/SpecialAuction/AuctionEndPopup.js
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react';
 
 function AuctionEndPopup({ auction, handleClosePopup }) {
 
-  const WinnerIndex = auction.auctionDetailDto.WinnerIndex;
-  const WinningBid= auction.auctionDetailDto.WinningBid;
+  const winnerIndex = auction.auctionDetailDto.winnerIndex;
+  const winningBid= auction.auctionDetailDto.winningBid;
   const winnerNickname= auction.auctionDetailDto.winnerNickname;
   
   return (
@@ -13,7 +13,7 @@ function AuctionEndPopup({ auction, handleClosePopup }) {
           <h2>경매 마감 안내</h2>
           <p>{auction.productName} <br/>상품의 실시간 경매가 마감되었습니다.</p>
           <p id='SAsuccessfulBidderName'>최종 낙찰자 : {winnerNickname}님</p>
-          <p id='SAsuccessfulBidderPrice'>낙찰 금액 : {WinningBid}원</p>
+          <p id='SAsuccessfulBidderPrice'>낙찰 금액 : {winningBid}원</p>
           <p id='SAendAuctionMent'>참여해 주신 모든 분들께 감사드리며, <br/> 다음 경매에도 많은 관심 부탁드립니다!</p>
           <button className='SAendOkbttn' onClick={handleClosePopup}>확인</button>
         </div>
